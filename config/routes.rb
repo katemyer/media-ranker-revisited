74d363bdfafc8704cf1127fdf1ec62614a1ce40f
kate myer <kate.d.mangubat@gmail.com>
2020-06-19 11:56:45 -0700

    added omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Omniauth Login Route
+  get "/auth/github", as: "github_login"
+  # Omniauth Github callback route
+  get "/auth/:provider/callback", to: "users#create", as: "omniauth_callback"
+
   root "works#root"
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
